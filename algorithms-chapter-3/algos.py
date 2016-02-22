ee8c6ad332aa38b5adcdf72bb138201116d65793
Shaun <shaunhutchins829@gmail.com>
2016-02-22 14:36:51 -0800

    algorithms chapter now contrains a bubble search solution n^2 time

--- algorithms-chapter-3/algos.py
+++ algorithms-chapter-3/algos.py
@@ -1,3 +1,5 @@
+import random
+
 play_list = range(1,11)
 def linSearch(x, lis):
 	i = 0
@@ -8,7 +10,7 @@ def linSearch(x, lis):
 	else:
 		return 0
 
-print linSearch(9, play_list)
+#print linSearch(9, play_list)
 
 def binSearch(x, lis):
 	#first set up two end points
@@ -39,4 +41,24 @@ while loop is no longer true
 x == lis[4]
 	return 5 for fifth spot in list
 '''
-print binSearch(5,play_list)
+#print binSearch(5,play_list)
+
+
+'''
+Selectively compare elements in a list of size n to neighboring elements
+in outer scroll go through all the elements
+in the inner go through all the elements and compare to the next element
+if the next element is less than the first element swap it
+'''
+def bubSort(lis):
+	sortLis = lis[:]
+	for i in range(len(lis)):
+		for j in range(len(lis) -1):
+			if sortLis[j] > sortLis[j+1]:
+				temp = sortLis[j]
+				sortLis[j] = sortLis[j+1]
+				sortLis[j+1] = temp
+	return sortLis
+
+ranLis = [random.randint(1,100) for i in range(10)]
+print (str(ranLis) + "\n" + str(bubSort(ranLis)))
